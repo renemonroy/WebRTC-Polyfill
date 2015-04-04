c34e4e6508cd99bb0a877f6f17fd927f412a4a05
René Monroy <rene.monroy@gmail.com>
2015-04-04 00:37:07 -0600

    add simple fallback for old implementations

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # WebRTC-Polyfill
 A tiny library that uses specific implementations of WebRTC on old browsers.
-
-Not ready yet...
--- dist/webrtc-polyfill.js
+++ dist/webrtc-polyfill.js
@@ -1,3 +1,77 @@
 (function(win, doc) {
   'use strict';
+
+  var polyWebRTC = function(config) {
+    this.init.apply(this, config);
+  };
+
+  polyWebRTC.prototype = {
+
+    constructor : polyWebRTC,
+    stunServer : null,
+
+    init : function(config) {
+      for ( var prop in config ) {
+        this[prop] = config[prop];
+      }
+      this._setRTC(navigator || null);
+      if ( !this.stunServer ) console.log('STUN missed, Mozilla requires ip.');
+    },
+
+    _setRTC : function(nav) {
+      if ( nav.getUserMedia ) {
+        _configStd(nav);
+      } else if (nav.webkitGetUserMedia) {
+        _configWebkit(nav);
+      } else if (nav.mozGetUserMedia) {
+        _configMozilla(nav);
+      } else {
+        console.log('WebRTC is not supported.');
+        this.isSupported = false;
+      }
+      return this;
+    },
+
+    _configStd : function(nav) {
+      var poly = this;
+      this.connectStreamToSrc = function(mediaStream) {
+        poly.mediaEl.srcObject = mediaStream;
+        poly.mediaEl.play();
+      };
+      this.rtcPeerConnection = RTCPeerConnection;
+      this.rtcSessionDescription = RTCSessionDescription;
+      this.getUserMedia = nav.getUserMedia.bind(nav);
+      this.isSupported = true;
+      return this;
+    },
+
+    _configWebkit : function(nav) {
+      var poly = this;
+      this.connectStreamToSrc = function(mediaStream) {
+        poly.mediaEl.src = webkitURL.createObjectURL(mediaStream);
+      };
+      this.rtcPeerConnection = webkitRTCPeerConnection;
+      this.rtcSessionDescription = RTCSessionDescription;
+      this.getUserMedia = nav.webkitGetUserMedia.bind(nav);
+      this.isSupported = true;
+      return this;
+    },
+
+    _configMozilla : function(nav) {
+      var poly = this;
+      this.connectStreamToSrc = function(mediaStream) {
+        poly.mediaEl.mozSrcObject = mediaStream;
+        poly.mediaEl.play();
+      };
+      this.rtcPeerConnection = mozRTCPeerConnection;
+      this.rtcSessionDescription = mozRTCSessionDescription;
+      this.getUserMedia = nav.mozGetUserMedia.bind(nav);
+      this.isSupported = true;
+      return this;
+    }
+
+  };
+
+  win.PolyWebRTC = polyWebRTC;
+
 })(window, document);
